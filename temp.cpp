46965b040ade191e538b29235d0cef19eb415e50
SPK-RPI <Shibaprasadkar50@gmail.com>
2017-02-07 23:22:10 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -2,17 +2,12 @@
 #include <fstream>
 using namespace std;
 int main(){
-    for (;;){
-  int a,b;
-  
-    ifstream f;
-    f.open ("/sys/class/thermal/thermal_zone0/temp");
-  
-    while (f>>a){}
-    
-    b=a/1000;
+     int a,b;
+  ifstream f;
+   f.open ("/sys/class/thermal/thermal_zone0/temp");
+   while (f>>a){}
+   b=a/1000;
       cout <<b<<" C'"<<endl;
-    }
 cin.get ();
 return 0
 }

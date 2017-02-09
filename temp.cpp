007cda0a0bc8cf74560519ddc92536d2a22d5632
SPK-RPI <shibaprasadkar50@gmail.com>
2017-02-09 05:33:40 +0000

    he

--- temp.cpp
+++ temp.cpp
@@ -9,7 +9,7 @@ int main(){
    f.open ("/sys/class/thermal/thermal_zone0/temp");
    while (f>>a){}
    b=a/1000;
-      cout <<"CPU===>> "b<<" C'"<<endl;
+      cout <<"CPU===>> "<<b<<" C'"<<endl;
 cin.get ();
-return 0
+return 0 ;
 }

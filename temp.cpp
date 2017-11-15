649b50d92be8a4e0f97efefd651c6fcf06cd252d
SPK-RPI <shibaprasadkar50@gmail.com>
2017-11-15 07:42:20 +0530

    color

--- temp.cpp
+++ temp.cpp
@@ -2,14 +2,26 @@
 // by SPK
 #include <iostream>
 #include <fstream>
+#include "color.h"
 using namespace std;
+
 int main(){
      int a,b;
   ifstream f;
    f.open ("/sys/class/thermal/thermal_zone0/temp");
    while (f>>a){}
    b=a/1000;
-      cout <<"CPU===>> "<<b<<" C'"<<endl;
+      if(b>=0&&b<=30){
+            cout <<FGRN("CPU===>> ")<<b<<" C'"<<endl;
+      }
+      else if (b>=31&&b<=49){
+            cout <<FYEL("CPU===>> ")<<b<<" C'"<<endl;
+                        
+      }
+      else{
+            cout <<FRED("CPU===>> ")<<b<<" C'"<<endl;
+      }
+
 cin.get ();
 return 0 ;
 }
